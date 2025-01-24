6cf359cbac6364cbc31e720dd348478cf131d20b
Mirza Mahrab Hossain <mahrjose.dev@gmail.com>
2025-01-24 17:57:28 +0600

    chore: remove frontend for now

--- client/src/utils/ErrorHandler.js
+++
@@ -1,9 +0,0 @@
-import { toast } from 'react-hot-toast';
-
-const handleAxiosError = (error) => {
-    const errorMessage = error?.response?.data?.message || "An unexpected error occurred.";
-    toast.error(errorMessage);
-    console.error(error);
-};
-
-export default handleAxiosError;
