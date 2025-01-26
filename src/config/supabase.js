41972c7db995812ae90b65cc93263089ec8083ec
Mirza Mahrab Hossain <mahrjose.dev@gmail.com>
2025-01-26 14:10:22 +0600

    feat: update Supabase client initialization to support dynamic environment variables based on NODE_ID

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,16 +1,20 @@
 const { createClient } = require("@supabase/supabase-js");
 const dotenv = require("dotenv");
-const logger = require("./logger"); // Import the logger
+const logger = require("./logger");
 
 dotenv.config();
 
-// Initialize Supabase client
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
-);
+// SUPABASE_URL = process.env.HQ_SUPABASE_URL
+// SUPABASE_KEY = process.env.HQ_SUPABASE_KEY
 
-// Test connection
+const NODE_ID = process.env.NODE_ID
+
+const SUPABASE_URL = process.env[`${NODE_ID}_SUPABASE_URL`];
+const SUPABASE_KEY = process.env[`${NODE_ID}_SUPABASE_KEY`];
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+// Test connection 
 (async () => {
   try {
     const { error } = await supabase.from("users").select("*").limit(1);
@@ -18,8 +22,7 @@ const supabase = createClient(
       logger.error("Unable to connect to Supabase:", error.message);
       throw new Error("Supabase connection failed");
     } else {
-      logger.info("Successfully connected to Supabase!");
-      // console.log("Successfully connected to Supabase!");
+      logger.info(`Successfully connected to Supabase! [${NODE_ID} - URL=> ${SUPABASE_URL}]`);
     }
   } catch (err) {
     logger.error("Supabase connection error:", err.message);
